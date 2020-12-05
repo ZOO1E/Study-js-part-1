86f7b59ffa181cc55a268ced7d1dce4954e2721c
ZOO1E <73783859+ZOO1E@users.noreply.github.com>
2020-12-05 23:15:41 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let js = 'amazing';
 if (js === 'amazing') alert('javascript is FUN!');
 
-
+40 + 8 + 23 - 10
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+let myFirstjob = 'Programmer';
+let myCurrentJob = 'Teacher' ;
+
+let job1 ='programmer';
+let job2 ='teacher';
+
+console.log(myFirstjob);
