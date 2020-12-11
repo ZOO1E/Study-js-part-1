87893d41e444ea382f3c58999541b04979227766
ZOO1E <73783859+ZOO1E@users.noreply.github.com>
2020-12-11 23:11:03 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ multiple \n\
 lines');
 console.log('String 
 multiple  // 이것도 안됨 ㅋㅋㅋㅋㅋㅋㅋㅋ
-lines'); */
+lines'); 
 
 const age = 19;
 
@@ -106,3 +106,49 @@ if(birthyear <=2000){
 }
 
 console.log(century);
+
+//type conversion
+const inputYear = '1991'; // string으로 인식
+console.log(Number(inputYear),inputYear);//여전히  inputYear는 string이고 Number라는 함수로 convert한거임
+console.log(Number(inputYear+18)); // 199118 -> 2009
+
+console.log(Number('Jonas'));// NaN
+console.log(typeof NaN); // number
+
+console.log(String(23),23); // 23을 string으로 인식
+
+//type coersion
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + String(23) + ' years old');
+console.log('23' -'10' - 3); //minus triggers 
+console.log('23' +'10' + 3);//23103
+console.log('23' * '2'); //46
+console.log('23' > '18');// string to number
+let n = '1' + 1 ; // string로 인식
+n = n -1; // number로 인식
+console.log(n); //10 연산자에 따라서 string과 number가 달라짐!! */
+
+//21.Truthy and Faisy Values
+//5 falsy valuse:0 , '' ,undefined, null , NaN
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); //flase
+console.log(Boolean('Jonas'));//true
+console.log(Boolean({}));//true
+console.log(Boolean(''));
+
+const money = 0; //0 is flase 
+if (money){
+     console.log("Don't spend it all ;)");
+}
+else {
+          console.log('you should het a job')
+     }
+let height ; // undefined
+if (height){
+     console.log('Yay! Height is defined');
+}else{
+     console.log('Height is undefined!')
+}
+
+
+
