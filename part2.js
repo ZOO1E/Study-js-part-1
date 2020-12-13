e280929ad45d1ae8ba6e1b1a99419cd6a6a61569
ZOO1E <73783859+ZOO1E@users.noreply.github.com>
2020-12-13 09:37:01 +0900

    Add files via upload

---
+++ part2.js
@@ -0,0 +1,12 @@
+//32.Activating strict mode
+'use strict' ; //secure code (to avoid error)
+
+let hasDriversLicense = false;
+const passTest =true;
+
+if (passTest) hasDriverLicense =true; 
+if(hasDriversLicense) console.log('i can drive :D')
+//const interface ='audio';
+//const private = 534 ; 
+//const if = 'ddf';  이런거 안됨 선택자는 변수가 될 수 없음!
+ 
